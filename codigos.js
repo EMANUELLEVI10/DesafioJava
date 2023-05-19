370309f53c0c4eeba4ab9f76ac127fe1dd3a3648
Emanuel Levi <130600784+EMANUELLEVI10@users.noreply.github.com>
2023-05-19 09:00:49 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,22 @@
+
+
+
+function ativarNome(){
+    let nome = 'Levi';
+    console.log(nome)
+     
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+
+function somarNumeros(){
+    let a = 9;
+    let b = 3;
+    let c = a*b;
+
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
